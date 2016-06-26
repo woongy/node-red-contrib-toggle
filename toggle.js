4270b7dee15e4c41faaca60a3e8906b289004ad6
Woongcheol Yang <woongcheol@gmail.com>
2016-06-26 17:04:24 +0900

    Add duration after which toggle will turn off

--- toggle.js
+++ toggle.js
@@ -4,6 +4,8 @@ module.exports = function(RED) {
   function ToggleNode(config) {
     RED.nodes.createNode(this, config);
     this.open = config.initial;
+    this.duration = Number(config.duration);
+    this.timeout = null;
 
     var node = this;
 
@@ -15,13 +17,27 @@ module.exports = function(RED) {
       }
     });
 
+    node.on("close", function() {
+      if (node.timeout) {
+        clearTimeout(node.timeout);
+        delete node.timeout;
+      }
+    })
+
     function toggleOn() {
       node.open = true;
       node.status({ fill: "green", shape: "dot", text: "on" });
+      if (node.duration && !node.timeout) {
+        node.timeout = setTimeout(toggleOff, node.duration * 1000);
+      }
     }
 
     function toggleOff() {
       node.open = false;
+      if (node.timeout) {
+        clearTimeout(node.timeout);
+        delete node.timeout;
+      }
       node.status({ fill: "grey", shape: "dot", text: "off" });
     }
 
